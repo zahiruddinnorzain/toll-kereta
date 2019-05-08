74b8ae8e835564afd2d0f9e7ea8d652861531071
zahiruddin <kepaknaga@gmail.com>
2019-05-08 14:02:53 +0800

    init
    first add

---
+++ vehicle_detection.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+print(cv2.__version__)
+
+cascade_src = 'cars.xml'
+video_src = 'dataset/video2.avi'
+#video_src = 'dataset/video2.avi'
+font = cv2.FONT_HERSHEY_SIMPLEX # font tulisan
+
+cap = cv2.VideoCapture(video_src)
+car_cascade = cv2.CascadeClassifier(cascade_src)
+
+while True:
+    ret, img = cap.read()
+    if (type(img) == type(None)):
+        break
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+    for (x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        
+        # write text, size 0.5, black(0,0,0), tebal=1
+        cv2.putText(img,'kereta',(x,y), font, 0.5,(0,0,0),1,cv2.LINE_AA)
+    
+    cv2.imshow('video', img)
+    
+    if cv2.waitKey(33) == 27: # Esc Key
+        break
+
+cv2.destroyAllWindows()
