05a72f3654157a8570ff2fd71a4ecc0b1704c175
zahiruddinnorzain <kepaknaga@gmail.com>
2019-05-13 17:19:14 +0800

    add bus xml and new file for 2 vehicle

--- vehicle_detection_2.py
+++ vehicle_detection_2.py
@@ -3,7 +3,8 @@
 import cv2
 print(cv2.__version__)
 
-cascade_src = 'cascade/cascade-old.xml'
+cascade_src = 'cascade/cars.xml'
+cascade_src2 = 'cascade/cascade-old.xml'
 video_src = 'dataset/video2.avi'
 #video_src = 'dataset/video2.avi'
 font = cv2.FONT_HERSHEY_SIMPLEX # font tulisan
@@ -11,6 +12,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX # font tulisan
 #cap = cv2.VideoCapture(video_src)
 cap = cv2.VideoCapture(0)
 car_cascade = cv2.CascadeClassifier(cascade_src)
+bus_cascade = cv2.CascadeClassifier(cascade_src2)
 
 while True:
     ret, img = cap.read()
@@ -19,13 +21,23 @@ while True:
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    cars = car_cascade.detectMultiScale(gray, 6.0, 1) # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1) # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    bus = bus_cascade.detectMultiScale(gray, 6, 1)
 
+    # kotak for car
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         
         # write text, size 0.5, black(0,0,0), tebal=1
         cv2.putText(img,'kereta',(x,y), font, 0.5,(0,0,0),1,cv2.LINE_AA)
+
+    # kotak for cbus
+    for (x,y,w,h) in bus:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        
+        # write text, size 0.5, black(0,0,0), tebal=1
+        cv2.putText(img,'bas',(x,y), font, 0.5,(0,0,0),1,cv2.LINE_AA)
+
     
     cv2.imshow('video', img)
     
