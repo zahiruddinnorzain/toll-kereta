226b52217dc7ab24539d2e3f65f7f8a8b7a10719
zahiruddinnorzain <kepaknaga@gmail.com>
2019-05-16 15:29:54 +0800

    add cascade and new test video

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -3,7 +3,7 @@
 import cv2
 print(cv2.__version__)
 
-cascade_src = 'cascade/cascade-old.xml'
+cascade_src = 'cascade/Bus_front.xml'
 video_src = 'dataset/video2.avi'
 #video_src = 'dataset/video2.avi'
 font = cv2.FONT_HERSHEY_SIMPLEX # font tulisan
@@ -19,7 +19,7 @@ while True:
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    cars = car_cascade.detectMultiScale(gray, 6.0, 1) # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1) # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
--- vehicle_detection_2.py
+++ vehicle_detection_2.py
@@ -3,14 +3,21 @@
 import cv2
 print(cv2.__version__)
 
+
+# Load file
 cascade_src = 'cascade/cars.xml'
 cascade_src2 = 'cascade/Bus_front.xml'
-video_src = 'dataset/video3.mp4'
-#video_src = 'dataset/video2.avi'
-font = cv2.FONT_HERSHEY_SIMPLEX # font tulisan
+video_src = 'dataset/video6.MOV'
 
+# Font
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Type of video capture [0 for stream]
 cap = cv2.VideoCapture(video_src)
 #cap = cv2.VideoCapture(0)
+
+
+# code start
 car_cascade = cv2.CascadeClassifier(cascade_src)
 bus_cascade = cv2.CascadeClassifier(cascade_src2)
 
@@ -21,27 +28,28 @@ while True:
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1) # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-    bus = bus_cascade.detectMultiScale(gray, 6, 1)
+    cars = car_cascade.detectMultiScale(gray, 1.9, 1) #2 # cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    bus = bus_cascade.detectMultiScale(gray, 1.1, 1) #1.1
 
     # kotak for car
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         
         # write text, size 0.5, black(0,0,0), tebal=1
-        cv2.putText(img,'kereta',(x,y), font, 0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(img,'Kereta RM 1',(x,y-5), font, 0.5,(0,255,0),1,cv2.LINE_AA)
 
-    # kotak for cbus
+    # kotak for bus
     for (x,y,w,h) in bus:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         
         # write text, size 0.5, black(0,0,0), tebal=1
-        cv2.putText(img,'bas',(x,y), font, 0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(img,'Bas RM 2',(x,y-5), font, 0.5,(0,255,0),1,cv2.LINE_AA)
 
-    
+
     cv2.imshow('video', img)
     
     if cv2.waitKey(33) == 27: # Esc Key
         break
 
+cap.release()
 cv2.destroyAllWindows()
